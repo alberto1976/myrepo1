998ca0b0e3a67bd259926054e2e24ac07c6444b9
Alberto Rodríguez <albrodriguezr@unal.edu.co>
2017-01-19 23:12:39 -0500

    Se adicionó la opción de crear tablas a través de la librería DT

--- Graficos.R
+++ Graficos.R
@@ -1,5 +1,14 @@
 # Un gráfico en ggplot2
 install.packages("ggplot2")
+
+# Gráfico ggplot2
+
 library(ggplot2)
 ggplot(iris, aes(x = Sepal.Length, y = Petal.Length, col = Species)) +
   geom_point(size = 3)
+
+# Tabla vía librería DT
+
+install.packages("DT")
+library(DT)
+datatable(head(iris,15))
